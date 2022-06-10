7400ad9bffc14353600622cf7ea40f5e57731538
GabrielOJorge <goliver331@protonmail.com>
2022-06-10 15:56:28 -0300

    create main-banner component

---
+++ src/components/main-banner/main-banner.js
@@ -0,0 +1,15 @@
+const bannerComponent = () => {
+  const banner = document.createElement('main');
+  const para = document.createElement('p');
+  const a = document.createElement('a');
+
+  para.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi alias at dolor illo sapiente.';
+  a.textContent = 'See the menu';
+  a.href = '#menu'
+
+  banner.append(para, a);
+
+  return banner;
+};
+
+export default bannerComponent;
