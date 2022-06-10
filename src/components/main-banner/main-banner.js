11ec79c6c0a37503e7a069502a8b516cb15476eb
GabrielOJorge <goliver331@protonmail.com>
2022-06-10 15:58:27 -0300

    set main-banner class to 'main-banner'

--- src/components/main-banner/main-banner.js
+++ src/components/main-banner/main-banner.js
@@ -6,6 +6,7 @@ const bannerComponent = () => {
   para.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi alias at dolor illo sapiente.';
   a.textContent = 'See the menu';
   a.href = '#menu'
+  banner.classList.add('main-banner');
 
   banner.append(para, a);
 
