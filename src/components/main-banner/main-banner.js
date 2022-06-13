51595ca156e7910a406057d97f75ffa07c8e9583
GabrielOJorge <goliver331@protonmail.com>
2022-06-13 15:35:36 -0300

    create a new paragraph in main-banner

--- src/components/main-banner/main-banner.js
+++ src/components/main-banner/main-banner.js
@@ -1,14 +1,18 @@
 const bannerComponent = () => {
   const banner = document.createElement('main');
-  const para = document.createElement('p');
+  const para1 = document.createElement('p');
+  const para2 = document.createElement('p');
   const a = document.createElement('a');
+  
+  para1.textContent = '123, Fake Street | Seattle, WA | 206-555-7890';
+  para2.textContent = 'Un bon repas doit commencer par la faim';
 
-  para.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi alias at dolor illo sapiente.';
   a.textContent = 'See the menu';
-  a.href = '#menu'
+  a.href = '#menu';
+
   banner.classList.add('main-banner');
 
-  banner.append(para, a);
+  banner.append(para1, para2, a);
 
   return banner;
 };
